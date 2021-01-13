e63a4465abc337d2cf22d20c7b85df9dcb8e66a1
HighIntensity19 <MrHighIntensity17@gmail.com>
2021-01-13 00:35:37 -0600

    2020-01-12 Search Bar

---
+++ StoreSearch/SearchResult.swift
@@ -0,0 +1,14 @@
+//
+//  SearchResult.swift
+//  StoreSearch
+//
+//  Created by Daniel Janes on 1/12/21.
+//
+
+import Foundation
+import UIKit
+
+class SearchResult {
+    var name = ""
+    var artistName = ""
+}
